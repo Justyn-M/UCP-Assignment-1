c6abcc60c5164dd9bbc60e555a32fc62d9c12be1
Justyn Mahen <justyn2s04mahen23@gmail.com>
2022-09-17 13:23:31 +0800

    player can move around map

--- main.c
+++ main.c
@@ -5,7 +5,6 @@
 #include "macros.h"
 #include "ops.h"
 
-
 char **generateMap(int mapDimensions[])
 {
     int row = mapDimensions[MAP_ROW];
@@ -54,7 +53,7 @@ void layMap(char **map, int mapDimensions[])
     }
 }
 
-void insertEntity(char** map, int playerPosition[], int goalPosition[])
+void insertEntity(char **map, int playerPosition[], int goalPosition[])
 {
     int playerRow = playerPosition[MAP_ROW];
     int playerCol = playerPosition[MAP_COL];
@@ -65,20 +64,19 @@ void insertEntity(char** map, int playerPosition[], int goalPosition[])
     map[goalRow][goalCol] = MARKER_GOAL;
 }
 
-static void printMap(char** map, int mapDimensions[])
+static void printMap(char **map, int mapDimensions[])
 {
     int row = mapDimensions[MAP_ROW];
     int col = mapDimensions[MAP_COL];
 
-    int i,j;
+    int i, j;
     for (i = 0; i < row; i++)
     {
-        for(j = 0; j < col; j++)
+        for (j = 0; j < col; j++)
         {
             printf("%c", map[i][j]);
         }
         printf("\n");
-
     }
 }
 
@@ -87,70 +85,68 @@ static void printMap(char** map, int mapDimensions[])
 =============================================================================
 =============================================================================*/
 
+/* main that allows command line arguments to be the mapDimensions, playerPosition and goalPosition */
 int main(int argc, char *argv[])
 {
-    int mapR, mapC, playerR, playerC, goalR, goalC;
-    char input;
-    int mapDimensions[] = {10, 10};
-    int playerPosition[] = {2, 2};
-    int goalPosition[] = {6, 6};
-    char **map = generateMap(mapDimensions);
+    char *Input = malloc(sizeof(char));
+    int mapDimensions[2];
+    int playerPosition[2];
+    int goalPosition[2];
 
     if (argc != 7)
     {
-        printf("./escape <row_map> <col_map> <row_player> <col_player> <row_goal> <col_goal>");
+        printf("Input: %s <map row> <map col> <player row> <player col> <goal row> <goal col>\n", argv[0]);
+        printf("Any out of bounds values will result in a segmentation fault\n");
         return 1;
     }
 
-    mapR = atoi(argv[1]);
-    mapC = atoi(argv[2]);
-    playerR = atoi(argv[3]);
-    playerC = atoi(argv[4]);
-    goalR = atoi(argv[5]);
-    goalC = atoi(argv[6]);
-
-    if (playerR == goalR && playerC == goalC)
-    {
-        printf("Invalid player/goal position\n");
-        printf("Player cannot be on the goal");
-        return 1;
-    }
-    else
-        printf("Input: %d %d %d %d %d %d\n", mapR, mapC, playerR, playerC, goalR, goalC);
-
-    /* if readuserinput is not w, a, s, d, then ask again and print 'wrong input */
-    
-    layMap(map, mapDimensions);
-    insertEntity(map, playerPosition, goalPosition);
-    printMap(map, mapDimensions);
-    wipeMap(map, mapDimensions);
-    readUserInput(&input);
-
-    while (input != UP && input != DOWN && input != LEFT && input != RIGHT)
-    {
-        printf("Wrong input - Only W,A,S,D allowed!\n");
-        printf("\n");
-        readUserInput(&input);
-    }
-
-    return 0;
-}
-
-
-/* test code 
-int main(int argc, char *argv[])
-{
-    int mapDimensions[] = {10, 10};
-    int playerPosition[] = {1, 1};
-    int goalPosition[] = {8, 8};
+    mapDimensions[MAP_ROW] = atoi(argv[1]);
+    mapDimensions[MAP_COL] = atoi(argv[2]);
+    playerPosition[MAP_ROW] = atoi(argv[3]);
+    playerPosition[MAP_COL] = atoi(argv[4]);
+    goalPosition[MAP_ROW] = atoi(argv[5]);
+    goalPosition[MAP_COL] = atoi(argv[6]);
 
     char **map = generateMap(mapDimensions);
+    system("clear");
     layMap(map, mapDimensions);
     insertEntity(map, playerPosition, goalPosition);
     printMap(map, mapDimensions);
     wipeMap(map, mapDimensions);
+    readUserInput(Input);
+
+    /* move player */
+
+    while (*Input != 'q')
+    {
+        if (*Input == UP)
+        {
+            playerPosition[MAP_ROW]--;
+        }
+        else if (*Input == DOWN)
+        {
+            playerPosition[MAP_ROW]++;
+        }
+        else if (*Input == LEFT)
+        {
+            playerPosition[MAP_COL]--;
+        }
+        else if (*Input == RIGHT)
+        {
+            playerPosition[MAP_COL]++;
+        }
+        else
+        {
+            printf("Invalid input\n");
+        }
+        map = generateMap(mapDimensions);
+        system("clear");
+        layMap(map, mapDimensions);
+        insertEntity(map, playerPosition, goalPosition);
+        printMap(map, mapDimensions);
+        wipeMap(map, mapDimensions);
+        readUserInput(Input);
+    }
 
     return 0;
 }
-
-*/
